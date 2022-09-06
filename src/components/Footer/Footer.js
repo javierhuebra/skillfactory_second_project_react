afd37e5550e521fdcadf2a035321cbe2a08c3215
javierhuebra <javieremanuelhuebra@gmail.com>
2022-09-06 02:22:58 -0300

    Feat: almost finished home section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,21 @@
-import '../../stylesheets/Footer/Footer.css'
+import '../../stylesheets/Footer/Footer.css';
+import { FaMapMarkerAlt, FaGithub, FaLinkedin } from "react-icons/fa";
+import imagenSkill from '../../images/Main/skill.png';
+import { BsMailbox} from "react-icons/bs";
 const Footer = () => {
     return(
         <div className="footer-container">
-
+            <div>
+                <p><FaMapMarkerAlt/>Bahía Blanca | Buenos Aires CP: 8000</p>
+                <p>Javier Emanuel Huebra</p>
+                <p>Skill Factory | By Avalith</p>
+            </div>
+            <div>
+            <a href='https://javierhuebra.github.io/skill_factory_first_project_js/'><p><FaGithub/>gitHub</p></a>
+            <a href='https://javierhuebra.github.io/skill_factory_first_project_js/'><p><FaLinkedin/>Linkedin</p></a>
+            <a href='https://javierhuebra.github.io/skill_factory_first_project_js/'><p><BsMailbox/>javieremanuelhuebra@gmail.com</p></a>
+            </div>
+            <a href='https://javierhuebra.github.io/skill_factory_first_project_js/'><img src={imagenSkill}/></a>
         </div>
     )
 }
--- src/components/Main/Slider.js
+++ src/components/Main/Slider.js
@@ -2,27 +2,58 @@ import '../../stylesheets/Main/Slider.css';
 import imagen_prueba from '../../images/Main/ricky.png';
 import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 import InfoSlider from './InfoSlider';
+import { useState } from 'react';
+
 const Slider = () => {
+
+
+    const [section, setSection] = useState(0);
+
+    const rightMove = () => {
+        section===2 ? setSection(0) : setSection(section+1);
+    }
+
+    const leftMove = () => {
+        section===0 ? setSection(2) : setSection(section-1);
+    }
+
+
     return(
         <div className='container-slider'>
             <div className='circles-container'>
-                <div className='circle circle-1'></div>
-                <div className='circle circle-2'></div>
-                <div className='circle circle-3'></div>
+                <div className={`circle circle-1 ${section===0 ? 'circulo-suave' : ''}`}></div>
+                <div className={`circle circle-2 ${section===1 ? 'circulo-suave' : ''}`}></div>
+                <div className={`circle circle-3 ${section===2 ? 'circulo-suave' : ''}`}></div>
             </div>
-            <div className='arrow left'><p><FaArrowCircleLeft/></p></div>
-            <div className='arrow right'><p><FaArrowCircleRight/></p></div>
+            <div className='arrow left' onClick={()=> leftMove()}><p><FaArrowCircleLeft/></p></div>
+            <div className='arrow right' onClick={()=> rightMove()}><p><FaArrowCircleRight/></p></div>
         <div className="view-port-slider">
-            <div className="container-images">
+
+            <div className={`container-images ${section===0 ? 'pos-0' : (section===1 ? 'pos-1' : 'pos-2')}`}>
                 <div className="image-1 component-slider">
                     <InfoSlider
                         title={'Rick & Morty'}
                         info={'A complete list of Rick and Morty characters fetched from Rick and Morty API.'}
                         btnText={'ENTER'}
-                        image={imagen_prueba}/>
+                        image={imagen_prueba}
+                    />
                 </div>
-                <div className="image-2 component-slider"></div>
-                <div className="image-3 component-slider"></div>
+                <div className="image-2 component-slider">
+                    <InfoSlider
+                        title={'Rick & Morty'}
+                        info={'A complete list of Rick and Morty characters fetched from Rick and Morty API.'}
+                        btnText={'ENTER'}
+                        image={imagen_prueba}
+                    />
+                </div>
+                <div className="image-3 component-slider">
+                    <InfoSlider
+                        title={'Rick & Morty'}
+                        info={'A complete list of Rick and Morty characters fetched from Rick and Morty API.'}
+                        btnText={'ENTER'}
+                        image={imagen_prueba}
+                    />
+                </div>
             </div>
             
         </div>
